f2d1bd7b35f6147ba32155e0b30fa36ff95aff05
Debojeet Das <debojeetofficial@gmail.com>
2025-02-02 07:39:38 +0530

    fix: added version and github link to navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,8 +1,18 @@
+<script>
+  import Icon from '@iconify/svelte'
+</script>
+
 <nav class="navbar bg-base-100 shadow-sm">
   <div class="flex-1">
-    <a class="btn btn-ghost text-xl" href="/">InstiAttend</a>
+    <a class="btn btn-ghost text-xl" href="/"
+      >InstiAttend
+      <span class="text-primary text-xs">v0.1</span>
+    </a>
   </div>
   <div class="flex-none">
+    <!-- github logo that leads to the repo -->
+    <a href="/" class="btn btn-ghost"> <Icon icon="akar-icons:github-fill" class="h-5 w-5" /></a>
+
     <!-- TA Profile -->
   </div>
 </nav>
